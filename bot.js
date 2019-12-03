38c35432a24322d59c26384447586b264c1e6870
Jason <jayjscotto@gmail.com>
2019-12-03 15:20:45 -0500

    tweet functionality

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var Twitter = require('twitter');
 var fs = require('fs');
 
@@ -8,23 +9,78 @@ var client = new Twitter({
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
 
 
+//tweet options for array
+var tweetList = [];
+
+//run a search for 'so cold'
+client.get('search/tweets', {q: 'so cold'}, function(error, tweets, response) {
+    
     //save tweets to json file
     //tweets come as objects, so we use JSON.stringify method
     //this process helps visualize what is coming in from twitter
-    fs.writeFile('tweets.json', JSON.stringify(tweets), (err) => {
+    fs.writeFile('tweets.json', JSON.stringify(tweets, null, '\t'), (err) => {
         if (err) throw err;
         console.log('New file saved!');
     });
-  }
-});
 
-//then run node bot.js
-//review JSON response
-//
+    //loop through results
+    for (tweet in tweets.statuses) {
+        
+        //save tweet's information to this object
+        let tweetObj = {};
+        tweetObj.tweet = tweets.statuses[tweet].text;
+        tweetObj.created_at = tweets.statuses[tweet].created_at
+        tweetObj.tweet_id = tweets.statuses[tweet].id;
+        tweetObj.url = tweets.statuses[tweet].url;
+        tweetObj.user = tweets.statuses[tweet].user.screen_name;
+        tweetObj.location =tweets.statuses[tweet].user.location;
+
+        //push the object to the array IF the tweet was created with a location
+        if (tweetObj.location) {
+            tweetList.push(tweetObj);
+        }
+    }
+
+    //select random array index
+    const generateRandom = (array) => {
+        return Math.floor(Math.random() * array.length + 1);
+    }
+
+    //generates random tweet for bot to put out
+    const tweetGen = (tweet) => {
+        console.log('1' + tweet)
+
+        //prepend tweet statements for each tweet
+        var statements = [
+            `It's super cold out in ${tweet.location}, thanks @${tweet.user}.`,
+            `We're getting word ${tweet.location} is cold AF, thanks @${tweet.user}.`,
+            `Bundle up, ${tweet.location}! @${tweet.user} says it's cold out.`,
+            `Planet Hoth or ${tweet.location}?? It's FREEZING according to @${tweet.user}`
+        ]
+        
+        randomStatement = generateRandom(statements);
+        return statements[randomStatement];
+    }
+
+    randomTweet = generateRandom(tweetList);
+    chosenTweet = tweetList[randomTweet];
+
+    //bot tweet
+    client.post('statuses/update', {status: tweetGen(chosenTweet)}, function(error, tweet, response) {
+        if (!error) {
+                console.log(tweet);
+                console.log('tweeted!')
+                }
+    });
+  
+    //writes a new file that includes only tweet info we have chosen/saved in tweetList
+    fs.writeFile('tweetlist.json', JSON.stringify(tweetList, null, '\t'), (err) => {
+        if (err) throw err;
+        console.log('New tweet list!');
+    });
+
+
+
+ });
